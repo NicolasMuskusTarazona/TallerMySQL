2b797817defe275fe825179a5c95bd6f9f9284cf
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-06-03 16:55:47 -0500

    feat: :sparkles: Obtén una lista de productos con un `precio mayor a $75 USD`, mostrando solo el `nombre`, el `precio` y su respectivo `precio en USD`.
    ConsultasBasicas.sql

--- ConsultasBasicas.sql
+++ ConsultasBasicas.sql
@@ -58,3 +58,8 @@ SELECT AVG(salario) FROM empleados;
 -- 10. Encuentra el número de productos en cada categoría, mostrando la categoría y el número de productos.
 
 SELECT categoria, COUNT(*) FROM productos GROUP BY categoria;
+
+-- 11. Obtén una lista de productos con un precio mayor a $75 USD, 
+-- mostrando solo el nombre, el precio y su respectivo precio en USD.
+SELECT nombre,precio / 4000 FROM productos
+WHERE (precio / 4000)>75;
