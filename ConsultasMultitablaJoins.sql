56c689a0ab5da769428ffd09fd6e15850df294ac
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-06-04 07:52:20 -0500

    feat: :sparkles: Lista todos los `productos pedidos junto con el precio unitario de cada pedido`.
    ConsultasMultitablaJoins.sql

--- ConsultasMultitablaJoins.sql
+++ ConsultasMultitablaJoins.sql
@@ -1,7 +1,13 @@
 -- Active: 1749036754683@@127.0.0.1@3307@familia
 -- 1. Encuentra los nombres de los clientes y los detalles de sus pedidos.
 
-SELECT usuarios.nombre, detalles_pedidos.pedido_id, detalles_pedidos.producto_id 
+SELECT DISTINCT usuarios.nombre, detalles_pedidos.pedido_id, detalles_pedidos.producto_id 
 FROM usuarios
 JOIN pedidos ON pedidos.cliente_id = usuarios.usuario_id
 JOIN detalles_pedidos ON detalles_pedidos.pedido_id = pedidos.pedido_id;
+
+-- 2. Lista todos los productos pedidos junto con el 
+-- precio unitario de cada pedido
+
+SELECT DISTINCT nombre,precio,estado FROM pedidos
+CROSS JOIN productos;
