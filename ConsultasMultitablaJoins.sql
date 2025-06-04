d22f162c602f2d4a24908def1ce4dafee8eb1d57
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-06-04 22:47:46 +0200

    feat: :sparkles: Encuentra los `nombres de los clientes` y los `nombres de los empleados` que gestionaron sus `pedidos`.
    ConsultasMultitablaJoins.sql

--- ConsultasMultitablaJoins.sql
+++ ConsultasMultitablaJoins.sql
@@ -1,4 +1,4 @@
--- Active: 1749036754683@@127.0.0.1@3307@taller
+-- Active: 1749069445371@@127.0.0.1@3307@taller
 -- 1. Encuentra los nombres de los clientes y los detalles de sus pedidos.
 
 SELECT DISTINCT usuarios.nombre, detalles_pedidos.pedido_id, detalles_pedidos.cantidad, detalles_pedidos.precio_unitario
@@ -9,14 +9,17 @@ JOIN detalles_pedidos ON detalles_pedidos.pedido_id = pedidos.pedido_id;
 -- 2. Lista todos los productos pedidos junto con el 
 -- precio unitario de cada pedido
 
-SELECT nombre,precio,estado FROM pedidos
-CROSS JOIN productos;
-
+SELECT p.pedido_id, GROUP_CONCAT(CONCAT(po.nombre,  ',',po.categoria)) AS productos, SUM(dp.cantidad * dp.precio_unitario) as Total
+FROM pedidos AS p
+INNER JOIN detalles_pedidos AS dp ON p.pedido_id = dp.pedido_id
+INNER JOIN productos AS po ON dp.producto_id = po.producto_id
+GROUP BY p.pedido_id;
 -- 3. Encuentra los nombres de los clientes y 
 -- los nombres de los empleados que gestionaron sus pedidos
 
-SELECT usuarios.nombre, pedidos.fecha_pedido, productos.nombre
-FROM usuarios
-INNER JOIN pedidos ON usuarios.usuario_id = pedidos.cliente_id
-INNER JOIN productos ON pedido_producto.id_producto = productos.id_producto;
+SELECT clientes.nombre AS cliente, empleados.nombre AS empleado, pedidos.fecha_pedido
+FROM pedidos
+JOIN usuarios AS clientes ON pedidos.cliente_id = clientes.usuario_id
+JOIN usuarios AS empleados ON pedidos.empleado_id = empleados.usuario_id;
+
 
